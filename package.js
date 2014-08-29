17fe0e455fa5e0f3f92de78e7e88c97ed33470a8
Chris Mather <mather.chris@gmail.com>
2014-08-29 11:55:06 -0700

    update package.js version info for new packaging system

--- package.js
+++ package.js
@@ -1,18 +1,17 @@
 Package.describe({
-  summary: "Allows you to define and run scheduled jobs across multiple servers."
+  summary: "Allows you to define and run scheduled jobs across multiple servers.",
+  version: "0.1.1"
 });
 
 Npm.depends({later: "1.1.6"});
 
 Package.on_use(function (api) {
   api.use(['underscore', 'check'], 'server');
-  
   api.add_files(['synced-cron-server.js'], "server");
-  
   api.export('SyncedCron', 'server');
 });
 
 Package.on_test(function (api) {
-  api.use(['synced-cron', 'tinytest']);
+  api.use(['percolatestudio:synced-cron', 'tinytest']);
   api.add_files('synced-cron-tests.js', ['server']);
 });

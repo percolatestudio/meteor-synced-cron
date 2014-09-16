50adfca227c107d02787e4a187b11073f6334099
Aaron Thorp <aaron@thinksoftware.net.au>
2014-09-16 23:01:14 +1000

    setup for mrt

--- package.js
+++ package.js
@@ -1,8 +1,5 @@
 Package.describe({
-  summary: "Allows you to define and run scheduled jobs across multiple servers.",
-  version: "0.3.1",
-  name: "aaronthorp:synced-cron",
-  git: "https://github.com/aaronthorp/meteor-synced-cron.git"
+  summary: "Allows you to define and run scheduled jobs across multiple servers."
 });
 
 Npm.depends({later: "1.1.6", sinon: "1.10.2"});

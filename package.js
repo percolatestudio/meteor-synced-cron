6d1812e36663d778e51cc6abffeae1545b6d0b53
Dan Dascalescu <ddascalescu+github@gmail.com>
2015-06-21 01:20:22 -0700

    Simplify & modernize

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  summary: "Allows you to define and run scheduled jobs across multiple servers.",
+  summary: "Define and run scheduled jobs across multiple servers.",
   version: "1.2.1",
   name: "percolate:synced-cron",
   git: "https://github.com/percolatestudio/meteor-synced-cron.git"
@@ -10,11 +10,11 @@ Npm.depends({later: "1.1.6"});
 Package.onUse(function (api) {
   api.versionsFrom('METEOR@0.9.1.1');
   api.use(['underscore', 'check', 'mongo', 'logging'], 'server');
-  api.add_files(['synced-cron-server.js'], "server");
+  api.addFiles(['synced-cron-server.js'], "server");
   api.export('SyncedCron', 'server');
 });
 
 Package.onTest(function (api) {
   api.use(['percolate:synced-cron', 'tinytest']);
-  api.add_files('synced-cron-tests.js', ['server']);
+  api.addFiles('synced-cron-tests.js', ['server']);
 });
